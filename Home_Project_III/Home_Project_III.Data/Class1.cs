a13142ec74514aaa3c2cf0726a47a725cff8f133
BomboM2001 <91763728+BomboM2001@users.noreply.github.com>
2022-07-24 17:34:25 +0700

    check

--- Home_Project_III/Home_Project_III.Data/Class1.cs
+++ Home_Project_III/Home_Project_III.Data/Class1.cs
@@ -2,7 +2,10 @@
 
 namespace Home_Project_III.Data
 {
+    // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\Database1.mdf";Integrated Security=True; MultipleActiveResultSets=True
+
     public class Class1
     {
+
     }
 }
